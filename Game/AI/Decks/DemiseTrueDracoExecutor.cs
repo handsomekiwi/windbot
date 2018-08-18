692e29408fbd49fbbbb707f44b240831c09d4ba5
handsomekiwi <television2288@gmail.com>
2018-08-18 14:02:06 +0800

    new

--- Game/AI/Decks/DemiseTrueDracoExecutor.cs
+++ Game/AI/Decks/DemiseTrueDracoExecutor.cs
@@ -10,41 +10,42 @@ namespace WindBot.Game.AI.Decks
     public class DemiseTrueDracoExecutor : DefaultExecutor
     {
         public class CardId
-        {/*            
-            public const int TheTrueDracombatant =;
-            public const int TheTrueDracofighter =;
-            public const int TheTrueDracocavalry =;
-            public const int TheTrueDracowarrior =;
-            public const int TheTrueDracocaster =;
-            public const int InspectorBoarder =;
-            public const int MaxxC =;
+        {            
+            public const int TheTrueDracombatant = 57761191;
+            public const int TheTrueDracofighter = 58984738;
+            public const int TheTrueDracocavalry = 94982447;
+            public const int TheTrueDracowarrior = 22499034;
+            public const int TheTrueDracocaster = 95004025;
+            public const int InspectorBoarder = 15397015;
+            public const int MaxxC = 23434538;
 
-            public const int CardOfDemise =;
-            public const int Scapegoat =;
-            public const int TheMonarchsStormfort =;
-            public const int TrueDracoHeritage =;
-            public const int DisciplesOfTheTrueDracophoenix =;
+            public const int CardOfDemise = 59750328;
+            public const int PotOfDuality = 98645731;
+            public const int Scapegoat = 73915051;
+            public const int TheMonarchsStormfort = 79844764;
+            public const int TrueDracoHeritage = 49430782;
+            public const int DisciplesOfTheTrueDracophoenix = 75425320;
 
-            public const int WakingTheDragon =;
-            public const int WaterfallOfDragonSouls =;
-            public const int VanityEmptiness =;
-            public const int TrueKingReturn =;
-            public const int TrueDracoApocalypse =;
-            public const int ImperialOrder =;
-            public const int PhantomKnightSword =;
+            public const int WakingTheDragon = 10813327;
+            public const int WaterfallOfDragonSouls = 23068051;
+            public const int VanityEmptiness = 5851097;
+            public const int TrueKingReturn = 35125879;
+            public const int TrueDracoApocalypse = 61529473;
+            public const int ImperialOrder = 61740673;
+            public const int PhantomKnightSword = 61936647;
 
-            public const int NaturiaExterio =;
-            public const int BloomDivaTheMelodiousChoir =;
-            public const int SuperheavySamuraiSteamTrainKing =;
-            public const int CrystalWingSynchroDragon =;
-            public const int RaidraptorUltimateFalcon =;
-            public const int BorreloadDragon =;
-            public const int BorrelswordDragon =;
-            public const int TheWorldChaliceWarrior =;
-            public const int KnightmarePhoenix =;
-            public const int KnightmareCerberus =;
-            public const int MissusRadiant =;
-            public const int Linkuriboh =;*/
+            public const int NaturiaExterio = 99916754;
+            public const int BloomDivaTheMelodiousChoir = 84988419;
+            public const int SuperheavySamuraiSteamTrainKing = 17775525;
+            public const int CrystalWingSynchroDragon = 50954680;
+            public const int RaidraptorUltimateFalcon = 86221741;
+            public const int BorreloadDragon = 31833038;
+            public const int BorrelswordDragon = 85289965;
+            public const int TheWorldChaliceWarrior = 30194529;
+            public const int KnightmarePhoenix = 2857636;
+            public const int KnightmareCerberus = 75452921;
+            public const int MissusRadiant = 3987233;
+            public const int Linkuriboh = 41999284;
         }
 
         public DemiseTrueDracoExecutor(GameAI ai, Duel duel)
