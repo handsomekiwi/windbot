86072cfa4eb1fc97f5247d0d7bf3b4cfe10e6efa
handsomekiwi <television2288@gmail.com>
2018-08-20 18:17:11 +0800

    .

--- Game/AI/Decks/DemiseTrueDracoExecutor.cs
+++ Game/AI/Decks/DemiseTrueDracoExecutor.cs
@@ -47,7 +47,7 @@ namespace WindBot.Game.AI.Decks
             public const int MissusRadiant = 3987233;
             public const int Linkuriboh = 41999284;
         }
-
+       
         public DemiseTrueDracoExecutor(GameAI ai, Duel duel)
             : base(ai, duel)
         {
@@ -60,6 +60,46 @@ namespace WindBot.Game.AI.Decks
             AddExecutor(ExecutorType.Activate, _CardId.AshBlossom, DefaultAshBlossomAndJoyousSpring);
             AddExecutor(ExecutorType.Activate, _CardId.GhostOgreAndSnowRabbit, DefaultGhostOgreAndSnowRabbit);
 
+            //chain
+            AddExecutor(ExecutorType.Activate, CardId.Scapegoat, Scapegoateff);
+            AddExecutor(ExecutorType.Activate, CardId.TheTrueDracofighter, TheTrueDracofightereff);
+            AddExecutor(ExecutorType.Activate, CardId.TheTrueDracocaster, TheTrueDracocastereff);
+            AddExecutor(ExecutorType.Activate, CardId.TheTrueDracowarrior, TheTrueDracowarrioreff);
+            AddExecutor(ExecutorType.Activate, CardId.TheTrueDracocavalry, TheTrueDracocavalryeff);
+
+            //sp
+            AddExecutor(ExecutorType.Activate, CardId.WakingTheDragon, WakingTheDragoneff);
+            AddExecutor(ExecutorType.SpSummon, CardId.BorrelswordDragon, BorrelswordDragonsp);
+            AddExecutor(ExecutorType.Activate, CardId.BorrelswordDragon, BorrelswordDragoneff);
+            AddExecutor(ExecutorType.SpSummon, CardId.MissusRadiant, MissusRadiantsp);
+            AddExecutor(ExecutorType.Activate, CardId.MissusRadiant, MissusRadianteff);
+            AddExecutor(ExecutorType.SpSummon, CardId.Linkuriboh);
+            AddExecutor(ExecutorType.Activate, CardId.Linkuriboh);
+            //normal summon            
+            AddExecutor(ExecutorType.Summon, CardId.InspectorBoarder, InspectorBoardersummon);
+            AddExecutor(ExecutorType.Summon, CardId.TheTrueDracombatant, TheTrueDracombatantsummon);
+
+            //spell set
+            AddExecutor(ExecutorType.SpellSet, CardId.Scapegoat, NoSetAlreadyDone);
+            AddExecutor(ExecutorType.SpellSet, CardId.ImperialOrder);
+            AddExecutor(ExecutorType.SpellSet, CardId.VanityEmptiness);
+            AddExecutor(ExecutorType.SpellSet, CardId.PhantomKnightSword);
+            AddExecutor(ExecutorType.SpellSet, CardId.WaterfallOfDragonSouls, NoSetAlreadyDone);
+            AddExecutor(ExecutorType.SpellSet, CardId.TheMonarchsStormfort, NoSetAlreadyDone);
+            AddExecutor(ExecutorType.SpellSet, CardId.WakingTheDragon, NormalSpellSet);
+            AddExecutor(ExecutorType.SpellSet, CardId.TrueDracoApocalypse);
+            AddExecutor(ExecutorType.SpellSet, CardOfDemiseSet);
+            AddExecutor(ExecutorType.Repos, MonsterRepos);
+        }
+        bool summon_used = false;
+        bool CardOfDemiseeff_used = false;
+        bool TheMonarchsStormforteff_used = false;
+        public override void OnNewTurn()
+        {
+            summon_used = false;
+            CardOfDemiseeff_used = false;
+            TheMonarchsStormforteff_used = false;
+            base.OnNewTurn();
         }
 
         private bool ToBattlePhaseeff()
@@ -75,5 +115,244 @@ namespace WindBot.Game.AI.Decks
             return false;
         }
 
+        private bool Scapegoateff()
+        {
+            if (DefaultSpellWillBeNegated()) return false;
+            if (Duel.Player == 0) return false;
+            if (Duel.Phase == DuelPhase.End) return true;
+            if (DefaultOnBecomeTarget()) return true;
+            if (Duel.Phase > DuelPhase.Main1 && Duel.Phase < DuelPhase.Main2)
+            {
+                int total_atk = 0;
+                List<ClientCard> enemy_monster = Enemy.GetMonsters();
+                foreach (ClientCard m in enemy_monster)
+                {
+                    if (m.IsAttack()) total_atk += m.Attack;
+                }
+                if (total_atk >= Bot.LifePoints) return true;
+            }
+            return false;
+        }
+
+        private bool TheTrueDracofightereff()
+        {
+            AI.SelectCard(CardId.TrueKingReturn);
+            AI.SelectOption(1);
+            return true;
+        }
+
+        private bool TheTrueDracocastereff()
+        {
+            AI.SelectOption(1);
+            return true;
+        }
+
+        private bool TheTrueDracowarrioreff()
+        {
+            AI.SelectCard(CardId.TheTrueDracofighter);
+            AI.SelectOption(1);
+            return true;
+        }
+
+        private bool TheTrueDracocavalryeff()
+        {
+            AI.SelectCard(new[] {
+                CardId.TheTrueDracombatant,
+                CardId.TheTrueDracocavalry,
+                CardId.TheTrueDracowarrior,
+                CardId.TheTrueDracocaster});
+            return true;
+        }
+
+        private bool WakingTheDragoneff()
+        {
+            if(Enemy.HasInMonstersZone(CardId.BorrelswordDragon) && Bot.HasInExtra(CardId.BorreloadDragon))
+            {
+                AI.SelectCard(CardId.BorreloadDragon);
+            }
+            else if (Enemy.HasInMonstersZone(CardId.BorrelswordDragon) && Bot.HasInExtra(CardId.SuperheavySamuraiSteamTrainKing))
+            {
+                AI.SelectCard(CardId.SuperheavySamuraiSteamTrainKing);
+                AI.SelectPosition(CardPosition.FaceUpDefence);
+            }
+            else 
+            AI.SelectCard(new[]
+            {
+                CardId.RaidraptorUltimateFalcon,
+                CardId.BorreloadDragon,
+                CardId.BorrelswordDragon,
+            });
+            return true;
+        }
+
+        private bool BorrelswordDragonsp()
+        {
+
+            if (!Bot.HasInMonstersZone(CardId.MissusRadiant))
+                return false;
+            IList<ClientCard> material_list = new List<ClientCard>();
+            foreach (ClientCard m in Bot.GetMonsters())
+            {
+                if (m.Id == CardId.MissusRadiant)
+                {
+                    material_list.Add(m);
+                    break;
+                }
+            }
+            foreach (ClientCard m in Bot.GetMonsters())
+            {
+                if (m.Id == CardId.Linkuriboh)
+                {
+                    material_list.Add(m);
+                    if (material_list.Count == 3)
+                        break;
+                }
+            }
+            if (material_list.Count == 3)
+            {
+                AI.SelectMaterials(material_list);
+                return true;
+            }
+            return false;
+        }
+
+        private bool BorrelswordDragoneff()
+        {
+            if (ActivateDescription == AI.Utils.GetStringId(CardId.BorrelswordDragon, 0))
+            {
+                if (AI.Utils.IsChainTarget(Card) && AI.Utils.GetBestEnemyMonster(true, true) != null)
+                {
+                    AI.SelectCard(AI.Utils.GetBestEnemyMonster(true, true));
+                    return true;
+                }
+                if (Duel.Player == 1 && Bot.BattlingMonster == Card)
+                {
+                    AI.SelectCard(Enemy.BattlingMonster);
+                    return true;
+                }
+                if (Duel.Player == 1 && Bot.BattlingMonster != null &&
+                    (Enemy.BattlingMonster.Attack - Bot.BattlingMonster.Attack) >= Bot.LifePoints)
+                {
+                    AI.SelectCard(Enemy.BattlingMonster);
+                    return true;
+                }
+                if (Duel.Player == 0 && Duel.Phase == DuelPhase.BattleStart)
+                {
+                    foreach (ClientCard check in Enemy.GetMonsters())
+                    {
+                        if (check.IsAttack() && !check.HasType(CardType.Link))
+                        {
+                            AI.SelectCard(check);
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private bool MissusRadiantsp()
+        {
+            IList<ClientCard> material_list = new List<ClientCard>();
+            foreach (ClientCard monster in Bot.GetMonsters())
+            {
+                if (monster.Level == 1)
+                    material_list.Add(monster);
+                if (material_list.Count == 2) break;
+            }
+            if (material_list.Count < 2) return false;
+            if (Bot.HasInMonstersZone(CardId.MissusRadiant)) return false;
+            AI.SelectMaterials(material_list);
+            if ((Bot.MonsterZone[0] == null || Bot.MonsterZone[0].Level == 1) &&
+                (Bot.MonsterZone[2] == null || Bot.MonsterZone[2].Level == 1) &&
+                Bot.MonsterZone[5] == null)
+                AI.SelectPlace(Zones.z5);
+            else
+                AI.SelectPlace(Zones.z6);
+            return true;
+        }
+
+        private bool MissusRadianteff()
+        {
+            AI.SelectCard(new[]
+           {
+                CardId.MaxxC,
+                CardId.MissusRadiant,
+            });
+            return true;
+        }
+
+        private bool Linkuribohsp()
+        {
+            foreach (ClientCard c in Bot.GetMonsters())
+            {
+                if (c.Level == 1)
+                {
+                    AI.SelectMaterials(c);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool Linkuriboheff()
+        {
+            if (Duel.LastChainPlayer == 0 && AI.Utils.GetLastChainCard().Id == CardId.Linkuriboh) return false;
+            return true;
+        }
+
+        private bool InspectorBoardersummon()
+        {
+            AI.SelectPlace(Zones.z1| Zones.z3);
+            summon_used = true;
+            return true;
+        }
+
+        private bool TheTrueDracombatantsummon()
+        {
+            return false;
+        }
+        private bool NoSetAlreadyDone()
+        {
+            if (Duel.Turn > 1 && Duel.Phase == DuelPhase.Main1 && Bot.HasAttackingMonster())
+                return false;
+            if (Bot.HasInSpellZone(Card.Id)) return false;
+            return true;
+        }
+
+        private bool NormalSpellSet()
+        {
+            if (Duel.Turn > 1 && Duel.Phase == DuelPhase.Main1 && Bot.HasAttackingMonster())
+                return false;
+            return true;
+        }
+
+        private bool CardOfDemiseSet()
+        {
+            if (Card.HasType(CardType.Field)) return false;
+            if (CardOfDemiseeff_used) return true;
+            if (Bot.HasInHandOrInSpellZone(CardId.CardOfDemise) && !CardOfDemiseeff_used)
+            {
+                int hand_spell_count = 0;
+                foreach (ClientCard s in Bot.Hand)
+                {
+                    if (s.HasType(CardType.Trap) || s.HasType(CardType.Spell) && !s.HasType(CardType.Field))
+                        hand_spell_count++;
+                }
+                int zone_count = 5 - Bot.GetSpellCountWithoutField();
+                return zone_count - hand_spell_count >= 1;
+            }           
+            return false;
+        }
+        public override bool OnSelectHand()
+        {
+            return true;
+        }
+
+        private bool MonsterRepos()
+        {            
+            return DefaultMonsterRepos();
+        }
     }
 }
